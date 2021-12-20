04288b16dc03fc2122f8c6b198cc226a85c0c925
aref hosseini <arefhosseini@yahoo.com>
2021-12-20 11:12:08 +0330

    service & repository of celebrity added to modules

--- app/src/main/java/com/fearefull/composemoviefetcher/di/RepositoryModule.kt
+++ app/src/main/java/com/fearefull/composemoviefetcher/di/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package com.fearefull.composemoviefetcher.di
 
+import com.fearefull.composemoviefetcher.data.remote.RepositoryCelebrity
 import com.fearefull.composemoviefetcher.data.remote.RepositoryMovie
+import com.fearefull.composemoviefetcher.data.remote.ServiceCelebrity
 import com.fearefull.composemoviefetcher.data.remote.ServiceMovie
 import dagger.Module
 import dagger.Provides
@@ -18,5 +20,13 @@ object RepositoryModule {
         service: ServiceMovie
     ): RepositoryMovie {
         return RepositoryMovie(service)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepositoryCelebrity(
+        service: ServiceCelebrity
+    ): RepositoryCelebrity {
+        return RepositoryCelebrity(service)
     }
 }
--- app/src/main/java/com/fearefull/composemoviefetcher/di/ServiceModule.kt
+++ app/src/main/java/com/fearefull/composemoviefetcher/di/ServiceModule.kt
@@ -1,11 +1,13 @@
 package com.fearefull.composemoviefetcher.di
 
+import com.fearefull.composemoviefetcher.data.remote.ServiceCelebrity
 import com.fearefull.composemoviefetcher.data.remote.ServiceMovie
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
@@ -16,5 +18,11 @@ object ServiceModule {
     @Singleton
     fun provideServiceMovie(retrofit: Retrofit): ServiceMovie {
         return retrofit.create(ServiceMovie::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideServiceCelebrity(retrofit: Retrofit): ServiceCelebrity {
+        return retrofit.create(ServiceCelebrity::class.java)
     }
 }
