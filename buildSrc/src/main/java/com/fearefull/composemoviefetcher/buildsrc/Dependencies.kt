34ef3c6bd8eceae57e50623e4338902b0cac3aa5
Aref Hosseini <arefhosseini@yahoo.com>
2022-08-28 21:58:58 +0430

    update library version

--- buildSrc/src/main/java/com/fearefull/composemoviefetcher/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/com/fearefull/composemoviefetcher/buildsrc/Dependencies.kt
@@ -2,25 +2,25 @@ package com.fearefull.composemoviefetcher.buildsrc
 
 object Dependencies {
     const val ktlint = "com.pinterest:ktlint:0.42.1"
-    const val coil = "io.coil-kt:coil-compose:2.0.0-alpha01"
-    const val daggerHiltVersion = "2.40.1"
+    const val coil = "io.coil-kt:coil-compose:2.2.0"
+    const val daggerHiltVersion = "2.43.2"
 
     object Kotlin {
-        const val version = "1.5.31"
+        const val version = "1.7.10"
         const val stdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${version}"
-        const val jsonSerialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0"
+        const val jsonSerialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0"
     }
 
     object Gradle {
-        const val androidBuildPlugin = "com.android.tools.build:gradle:7.2.0-alpha05"
+        const val androidBuildPlugin = "com.android.tools.build:gradle:7.2.2"
         const val hiltPlugin = "com.google.dagger:hilt-android-gradle-plugin:$daggerHiltVersion"
         const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
         const val kotlinSerializationPlugin = "org.jetbrains.kotlin:kotlin-serialization:${Kotlin.version}"
-        const val firebasePlugin = "com.google.gms:google-services:4.3.10"
+        const val firebasePlugin = "com.google.gms:google-services:4.3.13"
 
         object VersionsPlugin {
             const val id = "com.github.ben-manes.versions"
-            const val version = "0.39.0"
+            const val version = "0.42.0"
         }
     }
 
@@ -30,14 +30,15 @@ object Dependencies {
         const val dataStore = "androidx.datastore:datastore-preferences:1.0.0"
 
         object Ktx {
-            const val core = "androidx.core:core-ktx:1.7.0-rc01"
+            const val core = "androidx.core:core-ktx:1.8.0"
         }
     }
 
     object Compose {
         // Compose is combination of 7 Maven Group Ids within androidx.
         // Each Group contains a targeted subset of functionality, each with it's own set of release notes.
-        const val version = "1.1.0-beta03"
+        const val version = "1.2.1"
+        const val compilerVersion = "1.3.0"
         const val runtime = "androidx.compose.runtime:runtime:$version"
         const val foundation = "androidx.compose.foundation:foundation:$version"
         const val layout = "androidx.compose.foundation:foundation-layout:$version"
@@ -49,14 +50,14 @@ object Dependencies {
         const val tooling = "androidx.compose.ui:ui-tooling:$version"
         const val uiTest = "androidx.compose.ui:ui-test:$version"
         const val uiTestJunit = "androidx.compose.ui:ui-test-junit4:$version"
-        const val paging = "androidx.paging:paging-compose:1.0.0-alpha14"
-        const val activity = "androidx.activity:activity-compose:1.4.0"
-        const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07"
-        const val constraintLayout = "androidx.constraintlayout:constraintlayout-compose:1.0.0-rc01"
+        const val paging = "androidx.paging:paging-compose:1.0.0-alpha16"
+        const val activity = "androidx.activity:activity-compose:1.5.1"
+        const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0-alpha01"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout-compose:1.0.1"
 
         object Accompanist {
             private const val accompanistLibrary = "com.google.accompanist:accompanist"
-            private const val version = "0.20.0"
+            private const val version = "0.26.2-beta"
 
             const val insets = "$accompanistLibrary-insets:$version"
             const val insetsUi = "$accompanistLibrary-insets-ui:$version"
@@ -64,12 +65,12 @@ object Dependencies {
         }
 
         object Navigation {
-            private const val version = "2.4.0-beta02"
+            private const val version = "2.5.1"
             const val runtime = "androidx.navigation:navigation-compose:$version"
         }
 
         object HiltNavigation {
-            private const val version = "1.0.0-alpha03"
+            private const val version = "1.0.0"
             const val runtime = "androidx.hilt:hilt-navigation-compose:$version"
         }
     }
@@ -77,7 +78,6 @@ object Dependencies {
     object Hilt {
         const val android = "com.google.dagger:hilt-android:$daggerHiltVersion"
         const val androidCompiler = "com.google.dagger:hilt-android-compiler:$daggerHiltVersion"
-        const val viewmodel = "androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03"
         const val viewmodelCompiler = "androidx.hilt:hilt-compiler:1.0.0"
     }
 
@@ -86,14 +86,14 @@ object Dependencies {
         const val runtime = "com.squareup.retrofit2:retrofit:$version"
         const val gsonConverter = "com.squareup.retrofit2:converter-gson:$version"
 
-        const val interceptor = "com.squareup.okhttp3:logging-interceptor:4.9.1"
+        const val interceptor = "com.squareup.okhttp3:logging-interceptor:4.10.0"
     }
 
     object Test {
         const val junit = "junit:junit:4.13.2"
         const val mockk = "io.mockk:mockk:1.12.0"
-        const val striktAssertion = "io.strikt:strikt-core:0.32.0"
-        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2"
+        const val striktAssertion = "io.strikt:strikt-core:0.34.0"
+        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4"
         const val junitExt = "androidx.test.ext:junit:1.1.3"
     }
 
@@ -103,19 +103,19 @@ object Dependencies {
     }
 
     object Room {
-        private const val version = "2.3.0"
+        private const val version = "2.4.3"
         const val runtime = "androidx.room:room-runtime:$version"
         const val compiler = "androidx.room:room-compiler:$version"
     }
 
     object StartUp {
-        private const val version = "1.1.0"
+        private const val version = "1.1.1"
         const val runtime = "androidx.startup:startup-runtime:$version"
     }
 
     object Firebase {
-        private const val version = "21.0.1"
+        private const val version = "21.0.7"
         const val auth = "com.google.firebase:firebase-auth:$version"
-        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.2"
+        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4"
     }
 }
--- buildSrc/src/main/java/com/fearefull/composemoviefetcher/buildsrc/Versions.kt
+++ buildSrc/src/main/java/com/fearefull/composemoviefetcher/buildsrc/Versions.kt
@@ -6,8 +6,8 @@ package com.fearefull.composemoviefetcher.buildsrc
 
 object Versions {
     const val minSdk = 21
-    const val targetSdk = 31
-    const val compileSdk = 31
+    const val targetSdk = 33
+    const val compileSdk = 33
     const val versionCode = 1
     const val versionName = "0.0.1"
 }
