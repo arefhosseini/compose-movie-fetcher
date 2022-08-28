e1ab27514e130376bbdf5b02ac9c5c38ce9f21be
Aref Hosseini <arefhosseini@yahoo.com>
2022-08-28 21:59:29 +0430

    update coil fetching image

--- app/src/main/java/com/fearefull/composemoviefetcher/ui/components/CelebritiesGridItem.kt
+++ app/src/main/java/com/fearefull/composemoviefetcher/ui/components/CelebritiesGridItem.kt
@@ -6,14 +6,18 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BrokenImage
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
 import com.fearefull.composemoviefetcher.R
 import com.fearefull.composemoviefetcher.data.model.other.Celebrity
 import com.fearefull.composemoviefetcher.data.model.remote.GenderType
@@ -42,14 +46,19 @@ private fun CelebritiesGridItemThumbnail(
     description: String?,
     gender: GenderType
 ) {
-    val painter: Painter = url?.let { rememberImagePainter(
-        data = url
-    ) } ?: run { painterResource(
-        if (gender == GenderType.FEMALE) R.mipmap.female else R.mipmap.male
-    ) }
+    val painter = rememberAsyncImagePainter(
+        model = url,
+        error = rememberVectorPainter(Icons.Filled.BrokenImage),
+        placeholder = painterResource(id = if (gender == GenderType.FEMALE) R.mipmap.female else R.mipmap.male)
+    )
+    val colorFilter = when (painter.state) {
+        is AsyncImagePainter.State.Loading, is AsyncImagePainter.State.Error -> ColorFilter.tint(MaterialTheme.colors.primary)
+        else -> null
+    }
     Image(
         painter = painter,
         contentDescription = description,
+        colorFilter = colorFilter,
         modifier = Modifier
             .aspectRatio(ThemeConstants.MOVIES_GRID_ITEM_THUMBNAIL_ASPECT_RATIO, true)
             .clip(RoundedCornerShape(ThemeConstants.ROUNDED_CORNER.dp))
--- app/src/main/java/com/fearefull/composemoviefetcher/ui/components/MoviesGridItem.kt
+++ app/src/main/java/com/fearefull/composemoviefetcher/ui/components/MoviesGridItem.kt
@@ -6,14 +6,19 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BrokenImage
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
+import coil.compose.AsyncImagePainter.State.Error
+import coil.compose.AsyncImagePainter.State.Loading
+import coil.compose.rememberAsyncImagePainter
 import com.fearefull.composemoviefetcher.R
 import com.fearefull.composemoviefetcher.data.model.other.Movie
 import com.fearefull.composemoviefetcher.util.constants.ThemeConstants
@@ -45,14 +50,19 @@ private fun MoviesGridItemThumbnail(
     url: String?,
     description: String?
 ) {
-    val painter: Painter = url?.let { rememberImagePainter(
-        data = url
-    ) } ?: run { painterResource(
-        R.mipmap.movie
-    ) }
+    val painter = rememberAsyncImagePainter(
+        model = url,
+        error = rememberVectorPainter(Icons.Filled.BrokenImage),
+        placeholder = painterResource(R.mipmap.movie)
+    )
+    val colorFilter = when (painter.state) {
+        is Loading, is Error -> ColorFilter.tint(MaterialTheme.colors.primary)
+        else -> null
+    }
     Image(
         painter = painter,
         contentDescription = description,
+        colorFilter = colorFilter,
         modifier = Modifier
             .aspectRatio(ThemeConstants.MOVIES_GRID_ITEM_THUMBNAIL_ASPECT_RATIO, true)
             .clip(RoundedCornerShape(ThemeConstants.ROUNDED_CORNER.dp))
